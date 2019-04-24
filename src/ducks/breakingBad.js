df47d70377c10e4498de9b30c48731e14995af92
Jonathan May <jmay4826@gmail.com>
2019-04-23 23:37:13 -0500

    Initial commit

---
+++ src/ducks/breakingBad.js
@@ -0,0 +1,18 @@
+// Breaking Bad Ducks
+
+// Initial State
+
+const initialState = {
+  characters: [],
+  loading: true
+};
+
+// Action Types Go Here
+
+// Reducer
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
